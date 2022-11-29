49ac57f30ed6721a76488631d82af042d3d82906
varManWai <laizoke98@gmail.com>
2022-11-29 19:49:29 +0800

    added the state for the players and balance in the contract and display it out

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { manager: "" };
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+
   }
+
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -25,7 +31,11 @@ class App extends React.Component {
     return (
       <div>
         <h1>Lottery Contract</h1>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance)} ether!
+        </p>
       </div>
     );
   }
